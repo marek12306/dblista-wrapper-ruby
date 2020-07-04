1fd9bb5c9b9cad161a6af2c07830555a0638aa3f
deepivin <marek12306@gmail.com>
2020-07-04 14:07:06 +0200

    get_avatar docs

--- lib/dblista/helpers.rb
+++ lib/dblista/helpers.rb
@@ -2,6 +2,12 @@
 
 # Helper methods
 module DBLista::Helpers
+  # Crafts avatar url from user ID and avatar hash
+  #
+  # @param id [Integer] user ID
+  # @param hash [String] avatar hash
+  # @param size [Integer] avatar width (defaults to 2048)
+  # @return [Hash] raw data from DBLista
   def self.get_avatar(id, hash, size = 2048)
     "https://cdn.discordapp.com/avatars/#{id}/#{hash}?size=#{size}"
   end

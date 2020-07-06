a739c946d567efda06d0efd0df80619850fe09f9
deepivin <marek12306@gmail.com>
2020-07-06 13:34:34 +0200

    rate report

--- lib/dblista/user/rating.rb
+++ lib/dblista/user/rating.rb
@@ -34,5 +34,21 @@ module DBLista::User
       DBLista._delete("/#{type}s/#{id}/rate/#{target_id}", nil, @token)
       true
     end
+    # Reports rate of a selected bot/server
+    #
+    # @param id [Integer] entity ID
+    # @param target_id [Integer] target user to remove rate from (for owners only)
+    # @param report_reason [String] reason of report (details)
+    # @param type [Symbol] type of entity (bot/server)
+    # @return [Boolean] true if operation succeded
+    def report_rate(id, rate_id, report_reason = 'Brak powodu', type = :bot)
+      DBLista._validate_id id
+      raise DBLista::Error, DBLista::Errors::TYPE_NOT_ALLOWED unless DBLista::User::Client::ALLOWED_TYPES.include?(type)
+
+      DBLista._post("/#{type}s/#{id}/ratings/#{rate_id}/report", {
+                                                                   reportReason: report_reason.to_s
+                                                                 }, @token)
+      true
+    end
   end
 end
